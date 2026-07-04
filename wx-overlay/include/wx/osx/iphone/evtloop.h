e58cd1088378c1a8162ab730bf647de69287bab2
Toemeler <87390876+Toemeler@users.noreply.github.com>
2026-07-04 10:00:08 +0000

    step2: fix iphone evtloop.h - declare WakeUp (defined out-of-line in .mm), was inline dup
    Run-10: my inline WakeUp collided with evtloop.mm:110. Header now mirrors the
    .mm's method set exactly, all declarations only.

--- wx-overlay/include/wx/osx/iphone/evtloop.h
+++ wx-overlay/include/wx/osx/iphone/evtloop.h
@@ -2,6 +2,7 @@
 // Name:        wx/osx/iphone/evtloop.h
 // Purpose:     declaration of wxGUIEventLoop for wxOSX/iPhone (Orca-iOS-ipa)
 // Licence:     wxWindows licence
+// Matches the out-of-line definitions in src/osx/iphone/evtloop.mm exactly.
 ///////////////////////////////////////////////////////////////////////////////
 #ifndef _WX_OSX_IPHONE_EVTLOOP_H_
 #define _WX_OSX_IPHONE_EVTLOOP_H_
@@ -12,7 +13,7 @@ public:
     wxGUIEventLoop();
     ~wxGUIEventLoop();
 
-    virtual void WakeUp() override {}
+    virtual void WakeUp() override;
 
 protected:
     virtual int DoDispatchTimeout(unsigned long timeout) override;
